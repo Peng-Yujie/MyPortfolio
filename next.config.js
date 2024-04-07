8a3cbc85c65057c16ede0df8233606795af63a32
YJ_ <Pumbaaxx@icloud.com>
2024-04-07 10:12:53 -0700

    optimize next images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const withOptimizedImages = require("next-optimized-images");
+
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
@@ -10,4 +12,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withOptimizedImages(nextConfig);
